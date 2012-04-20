14d73c2eab9fe94929b91288c68a65564783da5c
Grant Rotskoff <gmr1887@gmail.com>
2012-04-20 12:30:27 -0500

    Hook Length Implemented

--- partitions.hs
+++ partitions.hs
@@ -14,8 +14,16 @@ partitions :: Int -> [Partition]
 partitions 0 = [Part []]
 partitions n = [ Part (i:xs) | i <- [1..n], (Part xs) <- partitions (n-i),
                         null xs || i >= (head xs)] 
-{-
-hookLength :: Partition -> Int
-hookLength a = (foldr1 (*) [1..length(a)]) `div` prodHooks where
-    prodHooks = undefined
--}
+
+-- For a list of ints in descending order
+
+hooks :: [Int] -> [Int]
+hooks [] = []
+hooks a = (hookList a) ++ (hooks dropColumn) where
+    dropColumn = (dropWhile (== 0)) $ (map (\s->s-1) a)    
+    hookList xs = [i+(xs!!i)|i<-[0..((length xs)-1)]]
+
+-- Calculate the dimension of a representation, via its Young Diagram
+-- We convert to arbitrary precision numbers because growth rates are fast. 
+dim :: Partition -> Integer
+dim (Part a) = (product $ map toInteger [1..(sum a)]) `div` (product $ map toInteger (hooks $ reverse a))
