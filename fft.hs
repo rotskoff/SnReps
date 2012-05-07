a7391095dfe92658cef7502bd28f83760f70e074
Grant Rotskoff <gmr1887@gmail.com>
2012-05-07 13:20:04 -0500

    FFT outline

--- fft.hs
+++ fft.hs
@@ -16,42 +16,8 @@ import Matrix
 import Functions
 import qualified Data.Map as Map 
 
--- Untyped for the moment:
--- Sum i = 1 .. n
--- Data Structures!
+-- Take an SnMap and return a function from a partition to a sum of matrices
 
 fft f _ (Part [1]) = eval f (Perm $ Map.fromList [(1,1)]) 
-fft f tau l = sum $ fftFactors where
-    fftFactors = [ (yor (i..n) l) (fft f' (adapt tau) l) | i <-[f1..n]] 
-     
+fft f tau l = undefined     
 
-
-
-{-
-Just some experiments with the C-T FFT...
-
-data Complex = C {real :: Double, imaginary :: Double} deriving (Eq)
-instance Show Complex where
-    show (C {real=a,imaginary=b}) = (show a) ++ " + " ++ (show b)++"i"
-
-
-data RealPolynomial = P [Double] deriving (Eq,Show)
-data ComplexPolynomial = PC [Complex] deriving (Eq,Show)
-
-fft :: RealPolynomial -> ComplexPolynomial
-fft (P (x:xs))
-    | null xs = P (map (\s -> C {real=x,imaginary=0}) (x:xs)
-    | 
-
--- Horrible and inefficient; just divide the number 
-powersof2 = map (^2) [1..]
-
-pad :: RealPolynomial -> RealPolynomial
-pad (P a)
-    | length (a) `elem` (powersof2) = (P a)
-    | otherwise = undefined
-
-
-factorContiguous :: [Int] -> [[Int]]
-factorContiguous a = [[i,i+1]| i <- a]
--}
